13ed243222649f9cf21ad1ed8e410b77ea5cc9a3
Tuomas Heiskanen <tuomas.heiskanen@observis.fi>
2019-02-20 17:17:07 +0200

    Add return type

--- index.d.ts
+++ index.d.ts
@@ -5,10 +5,10 @@ declare module "react-native-bluetooth-serial" {
     export function connect(id: string): Promise<number>;
     export function on(type: string, handler: any): void;
     export function withEncoding(encoding: string): void;
-    export function write(connectionId: number, data: string | Buffer);
+    export function write(connectionId: number, data: string | Buffer): void;
     export function isEnabled(): Promise<boolean>;
     export function enable(): Promise<void>;
-    export function writeToDevice(connectionId: number, base64: string);
+    export function writeToDevice(connectionId: number, base64: string): void;
     export function disconnect(connectionId: number): Promise<boolean>;
     export function reconnect(connectionId: number): Promise<number>;
 }
